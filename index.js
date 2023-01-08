010912651e4ec15fd342ae833bf35939921e75c6
iz.aia.h <imukisa024@gmail.com>
2023-01-08 15:43:52 +0300

    Finished full webpage layout

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ function openTab(tabName) {
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabName).classList.add("active-tab");
 }
+
+const sidemenu = document.getElementById("sidemenu");
+
+function openMenu() {
+  sidemenu.style.right = "0";
+}
+function closeMenu() {
+  sidemenu.style.right = "-200px";
+}
